11627888f11254ad700eafa2584c12b37c69c891
lamatmed <lamat032025@gmail.com>
2025-03-18 22:04:14 +0100

    Correction des warnings ESLint et optimisation useEffect

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,33 +1,44 @@
-"use client";
-import { FaFacebook, FaTwitter, FaLinkedin, FaGithub } from "react-icons/fa";
+'use client';
+
+import Link from "next/link";
 
 const Footer = () => {
-  const currentYear = new Date().getFullYear();
-
   return (
     <footer className=" w-full py-5 bg-gray-200 shadow-md">
-      <div className="flex flex-col md:flex-row items-center justify-between px-6 mx-auto max-w-7xl">
-        {/* Texte Copyright */}
-        <p className="text-sm text-center md:text-left text-black">
-  &copy; {currentYear } <span className="font-semibold">Stock-Local</span>. Tous droits réservés.
-  Développeur : <span className="font-semibold">Lamat Abdellahi</span>
-</p>
+			<div className="flex flex-col md:flex-row items-center justify-between px-6 mx-auto max-w-7xl">
+        <div className="flex flex-col items-center justify-between sm:flex-row">
+          {/* Logo et description */}
+          <div className="mb-4 sm:mb-0">
+            <Link href="/">
+              <span className="text-lg font-bold text-blue-900">Stock-local</span>
+            </Link>
+            <p className="mt-2 text-sm text-gray-700">
+              Simplifiez votre gestion avec notre application intuitive.
+            </p>
+          </div>
 
+          {/* Liens rapides */}
+          <div className="flex flex-wrap justify-center gap-6 text-sm">
+           
+            <Link href="/alerts" className="text-black hover:bg-blue-300 transition">
+              Alerts
+            </Link>
+            <Link href="/stock" className="text-black hover:bg-blue-300 transition">
+              Stock
+            </Link>
+            <Link href="/about" className="text-black hover:bg-blue-300 transition">
+              Infos
+            </Link>
+          </div>
+        </div>
 
-        {/* Icônes Réseaux Sociaux */}
-        <div className="flex space-x-4 mt-2 md:mt-0">
-          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-            <FaFacebook className="text-gray-800 hover:text-blue-600 text-xl transition" />
-          </a>
-          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-            <FaTwitter className="text-gray-800 hover:text-blue-400 text-xl transition" />
-          </a>
-          <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
-            <FaLinkedin className="text-gray-800 hover:text-blue-700 text-xl transition" />
-          </a>
-          <a href="https://github.com" target="_blank" rel="noopener noreferrer">
-            <FaGithub className="text-gray-800 hover:text-gray-900 text-xl transition" />
-          </a>
+        <div className="mt-6 border-t border-black pt-4 text-center">
+          <p className="text-xs text-gray-700">
+            © {new Date().getFullYear()}  <span className="text-primary font-bold">RIM TECHNOLOGIE.</span> Tous droits réservés.
+          </p>
+          <p className="text-xs text-gray-700">
+            Conçu par <span className=" font-bold">Lamat Abdellahi</span>.
+          </p>
         </div>
       </div>
     </footer>
